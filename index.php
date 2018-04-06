531caef087595fb906f6803c40dfe8a874da029c
Sandra Muller <smuller.diw@gmail.com>
2018-04-06 10:38:54 +0200

    ajoutcss

--- index.php
+++ index.php
@@ -5,8 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Site Démo</title>
+    <style>
+       h1{
+           position : relative ;
+           top : 50%;
+           left : 35%;
+           font-size : 30px;
+           color : blue;
+       }
+    </style>
 </head>
 <body>
-    <h1>BONJOUR</h1>
+    <h1>Hi everybody, it's friday !!! Have fun !</h1>
 </body>
 </html>
